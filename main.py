293648e002a32214a5702747c5fd026473465c95
typlosion14 <48457254+typlosion14@users.noreply.github.com>
2019-07-02 10:51:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import configparser as cp
 import os
-import pygame
-from urllib.request import urlopen, Request
-from PIL import Image
 
 class Config:
     TranslateConfig="en"
     Unstable=False
     InstanceNumber=1
     SteamAppsPath="D:\SteamLibrary\steamapps"
-    OriginPath=".\\"
+    OriginPath="D:\SteamLibrary\steamapps\common\Starbound\win64"
 class Translate:
     def template(self):
         if Config.TranslateConfig=="fr":
@@ -63,19 +60,13 @@ class Translate:
             return "Quit Starbound..."
 
 
-def source_html(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
-                             'AppleWebKit/537.11 (KHTML, like Gecko) '
-                             'Chrome/23.0.1271.64 Safari/537.11',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-               'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-               'Accept-Encoding': 'none',
-               'Accept-Language': 'en-US,en;q=0.8',
-               'Connection': 'keep-alive'}
-    return str(urlopen(Request(url=url, headers=headers)).read())
+
 def init():
     config = cp.ConfigParser()
-    config.read_file(open('config.ini'))
+    try:
+        config.read_file(open('config.ini'))
+    except:
+        input(os.getcwd())
     Config.OriginPath=os.getcwd()
     Config.InstanceNumber=int(len(config.sections()[1:]))
     try:
@@ -118,15 +109,11 @@ def LoadingInstance():
             dic['Instance'+str(i+1)]= config.get('INSTANCE'+str(i+1),'Name'),config.get('INSTANCE'+str(i+1),'WorshopList'),config.get('INSTANCE'+str(i+1),'ModsList')
     except:
         return Translate.ErrorInstance('')
-    return Translate.loading(''),dic
+    return ChooseYourInstance(dic)
 def directSearch(a_dir):
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
 def InstanceAppli(ch,dic):
-    pygame.quit()
-    #for i in range(0,len(dic)):
-    #    if dic['Instance'+str(i+1)][0]==ch:
-    #        InstanceLoad = dic['Instance'+str(i+1)]
     InstanceLoad=dic['Instance'+str(ch)]
     Workshop=InstanceLoad[1].split(',')
     ModLoad=InstanceLoad[2].split(',')
@@ -159,46 +146,9 @@ def ListeCreator(dic):
     for i in range(0,len(dic)):
         AllName.append(dic['Instance'+str(i+1)][0])
     return AllName
-def LaunchPygame(dic):
-    inProgress = True
-    # Initialisation de pygame
-    CYAN = (0, 255, 255)
-    pygame.init()
-    Fond_Ecran = pygame.display.set_mode((1000, 800))  # Taille de la fenetre (en pixel)
-    pygame.display.set_caption("Multibound Reborn")  # Nom de la fenetre
-    Font = pygame.font.Font(None, 32)
-    imageL=pygame.image.load(Config.OriginPath+"/textures/Fond.png")
-    Fond_Ecran.blit(imageL,(0,0))
-    Fond_Ecran.blit(Font.render("Choissisez une instance :", 1, CYAN), (1000/2-(11*len("Choissisez une instance :")), 0))
-
-    if int(Config.InstanceNumber/4)>=1:
-        for y in range(0,int(Config.InstanceNumber/4)):
-            for i in range(0,Config.InstanceNumber):
-                Fond_Ecran.blit(Font.render(dic['Instance'+str(i+1)][0], 1, CYAN), (250*i+1, y+1*64))
-    else:
-        for i in range(0, Config.InstanceNumber):
-            Fond_Ecran.blit(Font.render(dic['Instance' + str(i + 1)][0], 1, CYAN), (250 * i + 1, 64))
-    pygame.display.update()
-    while inProgress:
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                if int(Config.InstanceNumber / 4) >= 1:
-                    for y in range(0, int(Config.InstanceNumber / 4)):
-                        for i in range(0, Config.InstanceNumber):
-                            if (250 * i + 1)<pos[0]<(250 * (i + 2)) and (y + 1 * 64)<pos[1]<(y + 2 * 64+32):
-                                print(pos)
-                                return InstanceAppli(i+1, dic)
-                else:
-                    for i in range(0, Config.InstanceNumber):
-                        if (250 * i + 1) < pos[0] < (250 * (i + 2)) and (64) < pos[1] < (96):
-                            print(pos)
-                            return InstanceAppli(i + 1, dic)
-            if event.type == pygame.QUIT:
-                return False
 
 print(init())
 inst=LoadingInstance()
 print(inst[0])
-print(LaunchPygame(inst[1]))
+InstanceAppli(inst[1],inst[2])
 #Todo : Support Collection, Inteface Graphique avec Nom et Image, Createur d'instance
