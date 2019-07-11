8a43c521ccd458d5ecb739a95653e2c123fd0e55
typlosion14 <48457254+typlosion14@users.noreply.github.com>
2019-07-11 14:46:14 +0200

    MultiFiles for better organization

---
+++ CobraLib.py
@@ -0,0 +1,39 @@
+import os
+from urllib.request import urlopen, Request
+
+
+def source_html(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+                             'AppleWebKit/537.11 (KHTML, like Gecko) '
+                             'Chrome/23.0.1271.64 Safari/537.11',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+               'Accept-Encoding': 'none',
+               'Accept-Language': 'en-US,en;q=0.8',
+               'Connection': 'keep-alive'}
+    return str(urlopen(Request(url=url, headers=headers)).read())
+
+
+def directSearch(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+
+def ListToString(list):
+    string = ''
+    for i in range(len(list)):
+        if i != len(list) - 1:
+            string = string + str(list[i]) + ','
+        else:
+            string = string + str(list[i])
+    return string
+
+
+def StringToList(string):  # convertString
+    list = []
+    pos1 = 0
+    for i in range(string.count(",")):
+        list.append(string[pos1:string.index(",", pos1)])
+        pos1 = string.index(",", pos1) + 1
+    list.append(string[pos1:])
+    return list
---
+++ libCollectionSteam.py
@@ -0,0 +1,35 @@
+from CobraLib import source_html
+
+
+def Collection(collectionid):  # 1357125616
+    myStr = source_html("https://steamcommunity.com/sharedfiles/filedetails/?id=" + str(collectionid))
+    id = []
+    pos = 1
+    b = False
+    while pos != 0:
+        pos = myStr.find("<div class=\"workshopItem\">", pos) + 1
+        findStr = "<a href=\"https://steamcommunity.com/sharedfiles/filedetails/?id="
+        pos = myStr.find(findStr, pos) + len(findStr)
+        pos2 = myStr.find("\"><div class=\"workshopItemPreviewHolder", pos)
+        url = myStr[pos:pos2]
+        if url in id:
+            break
+        char = url[:1]
+        for i in range(0, 10):
+            if char == str(i):
+                b = True
+        if b == False:
+            break
+        if len(url) > 25:
+            break
+        id.append(url)
+    return id
+
+
+def TitleWorkshop(appid):  # 1790667104
+    data_html = source_html("https://steamcommunity.com/sharedfiles/filedetails/?id=" + str(appid))
+    pos1 = data_html.find('<div class="workshopItemTitle">') + len('<div class="workshopItemTitle">')
+    Title = data_html[pos1:data_html.find('</div', pos1)]
+    if len(Title) > 100:
+        return "Not Found"
+    return Title
