7f1db754dd47ae43baca8eb7e309cd7ce27abc75
typlosion14 <typlosion14@github.com>
2020-08-20 18:58:56 +0200

    Initial commit

---
+++ CobraLib.py
@@ -0,0 +1,89 @@
+import os
+from urllib.request import urlopen, Request
+import requests
+
+
+
+def source_html(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+                             'AppleWebKit/537.11 (KHTML, like Gecko) '
+                             'Chrome/23.0.1271.64 Safari/537.11',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+               'Accept-Encoding': 'none',
+               'Accept-Language': 'en-US,en;q=0.8',
+               'Connection': 'keep-alive'}
+    return str(urlopen(Request(url=url, headers=headers)).read())
+
+def importjson(url,auth_token=''):
+    get=requests.get(url,headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+                                 'AppleWebKit/537.11 (KHTML, like Gecko) '
+                                 'Chrome/23.0.1271.64 Safari/537.11',
+                                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                                 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                                 'Accept-Encoding': 'none',
+                                 'Accept-Language': 'en-US,en;q=0.8',
+                                 'Connection': 'keep-alive',
+                                 'Authorization': 'token '+auth_token})
+    return get.text
+
+
+def directSearch(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+def pakSearch(a_dir):
+    listdir=os.listdir(a_dir)
+    dirpak=[]
+    for i in range(len(listdir)):
+        if listdir[i].endswith(".pak"):
+            dirpak.append(listdir[i])
+    return dirpak
+
+
+def ListToString(list):
+    string = ''
+    for i in range(len(list)):
+        if i != len(list) - 1:
+            string = string + str(list[i]) + ','
+        else:
+            string = string + str(list[i])
+    return string
+
+
+def StringToList(string):  # convertString
+    list = []
+    pos1 = 0
+    for i in range(string.count(",")):
+        list.append(string[pos1:string.index(",", pos1)])
+        pos1 = string.index(",", pos1) + 1
+    list.append(string[pos1:])
+    return list
+def StringToDict(string):
+    #{"id":"5d1e07a0a38e4a4fdd7cefca","activation":"2019-07-19T13:00:00.000Z","startString":"-19m 10s","expiry":"2019-07-21T13:00:00.000Z","active":true,"character":"Baro Ki'Teer","location":"Larunda Relay (Mercury)","inventory":[{"item":"Kavat Bust","ducats":220,"credits":250000},{"item":"Scimitar Cydonia Skin","ducats":375,"credits":340000},{"item":"Jolt","ducats":300,"credits":150000},{"item":"Voltaic Strike","ducats":300,"credits":150000},{"item":"High Voltage","ducats":300,"credits":150000},{"item":"Shell Shock","ducats":300,"credits":150000},{"item":"Primed Quickdraw","ducats":375,"credits":120000},{"item":"Primed Heavy Trauma","ducats":350,"credits":100000},{"item":"Prova Vandal","ducats":410,"credits":250000},{"item":"Quanta Vandal","ducats":450,"credits":300000},{"item":"Quanta Aufeis Skin","ducats":300,"credits":300000},{"item":"Ki'Teer Diax Syandana","ducats":325,"credits":450000},{"item":"Ki'Teer Foros Shoulder Plates","ducats":310,"credits":100000},{"item":"Ki'Teer Foros Chest Plate","ducats":175,"credits":200000},{"item":"Ki'Teer Foros Leg Plates","ducats":225,"credits":150000},{"item":"Prisma Companion Poster","ducats":90,"credits":110000},{"item":"Pyra Sugatra","ducats":100,"credits":200000},{"item":"Tigris Elixis Skin","ducats":300,"credits":275000},{"item":"Sonicor Exilis Skin","ducats":380,"credits":175000},{"item":"Liset Prisma Skin","ducats":120,"credits":150000},{"item":"Ki'Teer Atmos Diadem","ducats":525,"credits":375000},{"item":"Baro Ki'Teer Glyph","ducats":80,"credits":50000},{"item":"Eminence","ducats":220,"credits":220000},{"item":"Corrupted Bombard Specter Blueprint","ducats":100,"credits":50000},{"item":"Sands Of Inaros Blueprint","ducats":100,"credits":25000}],"psId":"5d1e07a0a38e4a4fdd7cefca25","endString":"1d 23h 40m 49s"}
+    #{"id":"5d1e07a0a38e4a4fdd7cefca","activation":"2019-07-19T13:00:00.000Z","startString":"24m 19s","expiry":"2019-07-21T13:00:00.000Z","active":false,"character":"Baro Ki\'Teer","location":"Larunda Relay (Mercury)","inventory":[],"psId":"5d1e07a0a38e4a4fdd7cefca0","endString":"2d 24m 19s"}
+    dic={}
+    pos1=0
+    while string[pos1:].count(",")!=0:
+        delimiter1=string[pos1+1]
+        key=string[pos1+2:string.find(delimiter1,pos1+2)]
+        pos1=string.find(delimiter1+key+delimiter1+":")+len(delimiter1+key+delimiter1+":")
+        delimiter2=string[pos1:pos1+1]
+        if string[pos1:pos1+2]=="[{":
+            data=string[pos1:string.find("}]")+1]
+            pos1=pos1+len(data)+1
+        elif delimiter2 in ('"', "'"):
+            data=string[string.find(delimiter2,pos1)+1:string.find(delimiter2,pos1+2)]
+            pos1 = string.find(",", pos1)
+        else:
+            data=string[pos1:string.find(",",pos1)]
+            pos1 = string.find(",", pos1)
+        dic[key]=data
+    return dic
+def StringToDate(string):#2019-07-22T00:00:00.000Z
+    Year=string[:4]
+    Month=string[5:7]
+    Day=string[8:10]
+    Hour=string[11:13]
+    Minute=string[14:16]
+    return Year,Month,Day,Hour,Minute
---
+++ py/main.py
@@ -0,0 +1,156 @@
+import configparser as cp
+import webbrowser
+import os
+import json
+import shutil
+from CobraLib import directSearch, importjson, pakSearch
+from configStorage import Config, Translate, init
+from secret import key
+
+
+def ChooseYourInstance(dic):
+    try:
+        for i in range(0, len(dic)):
+            print(str(i + 1) + " " + dic['Instance' + str(i + 1)][0])
+    except:
+        return Translate.ErrorInstance()
+    try:
+        choice = int(input(Translate.ChooseInstanceMain()))
+        if choice not in range(1, len(dic) + 1):
+            return ChooseYourInstance(dic)
+        else:
+            return Translate.loading(), choice, dic
+    except:
+        return ChooseYourInstance(dic)
+
+
+def CheckUpdate():
+    DicGit = json.loads(importjson("https://api.github.com/repos/typlosion14/Multibound-Reborn/releases/latest", key()))
+    VersionGit = DicGit['tag_name']
+    config = cp.ConfigParser()
+    config.read_file(open('config.ini'))
+    ActualVersion = Config.Version
+    if ActualVersion != VersionGit and ActualVersion[0] <= VersionGit[0] and ActualVersion[2] <= VersionGit[2] and \
+            ActualVersion[4] <= VersionGit[4]:
+        print(Translate.NeedUpdate())
+        try:
+            ch = int(input(Translate.DownloadChoice()))
+        except:
+            return
+        if ch == 1:
+            UrlGit = DicGit['assets']['browser_download_url']
+            print(Translate.loading())
+            webbrowser.open_new(UrlGit)
+            input(Translate.PleaseInstall())
+            exit()
+    return Translate.NoUpdateFound()
+
+
+def LoadingInstance():
+    config = cp.ConfigParser()
+    config.read_file(open('config.ini'))
+    dic = {}
+    try:
+        for i in range(0, Config.InstanceNumber):
+            try:
+                dic['Instance' + str(i + 1)] = config.get('INSTANCE' + str(i + 1), 'Name'), config.get(
+                    'INSTANCE' + str(i + 1), 'workshoplist'), config.get('INSTANCE' + str(i + 1),
+                                                                         'ModsList'), config.get(
+                    'INSTANCE' + str(i + 1), 'saveLocation')
+            except:
+                dic['Instance' + str(i + 1)] = config.get('INSTANCE' + str(i + 1), 'Name'), config.get(
+                    'INSTANCE' + str(i + 1), 'workshoplist'), config.get('INSTANCE' + str(i + 1), 'ModsList'), 'default'
+    except:
+        return Translate.ErrorInstance()
+    return ChooseYourInstance(dic)
+
+
+def InstanceAppli(ch, dic):
+    InstanceLoad = dic['Instance' + str(ch)]
+    Workshop = InstanceLoad[1].split(',')
+    ModLoad = InstanceLoad[2].split(',')
+    saveLocation = InstanceLoad[3]
+    try:  # Changer les mods du workshop
+        direc = directSearch(Config.SteamAppsPath + "\\workshop\content\\211820")
+        os.chdir(Config.SteamAppsPath + "\\workshop\content\\211820")
+        for i in range(0, len(direc)):
+            if not direc[i].startswith("Disabled."):
+                VerifDirec = direc[i]
+                for y in range(0, len(direc)):
+                    if direc[y].startswith("Disabled.") and direc[y].endswith(VerifDirec):
+                        shutil.rmtree(Config.SteamAppsPath + "\\workshop\content\\211820\\Disabled." + VerifDirec)
+            if direc[i] not in Workshop and "Disabled." not in direc[i]:
+                os.rename(direc[i], "Disabled." + str(direc[i]))
+        for i in range(0, len(Workshop)):
+            if ("Disabled." + Workshop[i]) in direc:
+                os.rename("Disabled." + Workshop[i], Workshop[i])
+    except:
+
+        return Translate.WorkshopError()
+    try:  # changer les mods
+        direc = directSearch(Config.SteamAppsPath + "\\common\Starbound\mods") + pakSearch(
+            Config.SteamAppsPath + "\common\\Starbound\mods")
+        os.chdir(Config.SteamAppsPath + "\\common\Starbound\mods")
+        for i in range(0, len(direc)):
+            if not direc[i].startswith(".Disabled."):
+                VerifDirec = direc[i]
+                for y in range(0, len(direc)):
+                    if direc[y].startswith(".Disabled.") and direc[y].endswith(VerifDirec):
+                        shutil.rmtree(Config.SteamAppsPath + "\\common\Starbound\mods\\.Disabled." + VerifDirec)
+            if direc[i] not in ModLoad and "Disabled." not in direc[i]:
+                os.rename(direc[i], ".Disabled." + str(direc[i]))
+        for i in range(0, len(ModLoad)):
+            if (".Disabled." + ModLoad[i]) in direc:
+                os.rename(".Disabled." + ModLoad[i], ModLoad[i])
+    except:
+        return Translate.ModError()
+    if saveLocation != "default":
+        try:
+            os.makedirs(Config.SteamAppsPath + "\\common\Starbound\InstanceSave\\" + str(ch))
+        except:
+            print('Save found!')
+        try:
+            os.replace(Config.SteamAppsPath + "\\common\Starbound\\storage",
+                       Config.SteamAppsPath + "\\common\Starbound\InstanceSave\\storage")
+            os.replace(Config.SteamAppsPath + "\\common\Starbound\InstanceSave\\" + str(ch),
+                       Config.SteamAppsPath + "\\common\Starbound\\storage")
+        except:
+            return Translate.BugMoveFile()
+    print(Translate.LaunchStarbound())
+    os.system('"' + Config.SteamAppsPath + '\\common\Starbound\win64\starbound.exe"')  # Lance le jeu
+    # Annule les changements
+    direc = directSearch(Config.SteamAppsPath + "\\workshop\content\\211820")
+    os.chdir(Config.SteamAppsPath + "\\workshop\content\\211820")
+    for i in range(0, len(direc)):
+        if direc[i].startswith("Disabled."):
+            NewName = str(direc[i][direc[i].find('.') + 1:])
+            os.rename(direc[i], NewName)
+    direc = directSearch(Config.SteamAppsPath + "\\common\Starbound\mods") + pakSearch(
+        Config.SteamAppsPath + "\common\\Starbound\mods")
+    os.chdir(Config.SteamAppsPath + "\\common\Starbound\mods")
+    for i in range(0, len(direc)):
+        if direc[i].startswith(".Disabled."):
+            NewName = str(direc[i][direc[i].find('.Disabled.') + len(".Disabled."):])
+            os.rename(direc[i], NewName)
+    # SaveLocation
+    if saveLocation != "default":
+        os.replace(Config.SteamAppsPath + "\\common\Starbound\\storage",
+                   Config.SteamAppsPath + "\\common\Starbound\InstanceSave\\" + str(ch))
+        os.replace(Config.SteamAppsPath + "\\common\Starbound\InstanceSave\\storage",
+                   Config.SteamAppsPath + "\\common\Starbound\\storage")
+    return "See you later!"
+
+
+def ListeCreator(dic):
+    AllName = []
+    for i in range(0, len(dic)):
+        AllName.append(dic['Instance' + str(i + 1)][0])
+    return AllName
+
+
+print(CheckUpdate())
+print(init())
+inst = LoadingInstance()
+print(inst[0])
+print(InstanceAppli(inst[1], inst[2]))
+# Todo : Inteface Graphique avec Nom et Image
